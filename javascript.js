3d35f38201ba1c872ccb13114e34bdfed0de8727
kadenn04 <kadennguyen101@gmail.com>
2025-06-19 15:54:11 -0500

    Add resizing button and resizing properties
    Adjust js to allow for changing the number of grid squares.
    Adjust css to center the etch-a-sketch and to maintain size regardless of number of grid squares.
    Finish the button and prompting.

--- javascript.js
+++ javascript.js
@@ -2,24 +2,54 @@
 
 const container = document.querySelector("#container");
 
-for(i=0; i<16; i++) {
-    var gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row");
-    container.append(gridRow);
-    for (j=0;j<16;j++){
-        var gridSquare = document.createElement("div");
-        gridSquare.classList.add("grid-square");
-        gridRow.append(gridSquare);
+function createGrid(size) {
+    for(i=0; i<size; i++) {
+        var gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        container.append(gridRow);
+        for (j=0;j<size;j++){
+            var gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            gridRow.append(gridSquare);
+        }
     }
+
+    const gridSquares = document.querySelectorAll("div.grid-square");
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseover", () =>  {
+            square.style.backgroundColor = "red";
+        });
+        square.addEventListener("mouseover", () => {
+            console.log("HI!");
+        });
+    });
 }
 
-const gridSquares = document.querySelectorAll("div.grid-square");
+function deleteGrid() {
+    var gridSquares = document.querySelectorAll("div.grid-square");
+    gridSquares.forEach((square) => {
+        square.remove();
+    })
+    var gridRows = document.querySelectorAll("div.grid-row");
+    gridRows.forEach((row) => {
+        row.remove();
+    })
+}
 
-gridSquares.forEach((square) => {
-    square.addEventListener("mouseover", () =>  {
-        square.style.backgroundColor = "red";
-    });
-    square.addEventListener("mouseover", () => {
-        console.log("HI!");
-    });
-});
+
+function resetGrid() {
+    var newSize = Number(prompt("Number of squares per side for the new grid:", 16));
+    console.log(newSize);
+    if (newSize == 0 || newSize == NaN) {
+        return;
+    } else {
+        deleteGrid();
+        createGrid(newSize);
+    }
+
+}
+
+const promptButton = document.querySelector("#prompt-button");
+promptButton.addEventListener("click", resetGrid);
+
+createGrid(16);
